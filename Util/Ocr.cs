ea55b18cc0ae92a0fa716d47fd65b62a70cec0e3
laiwang <2799199459@qq.com>
2023-04-04 13:36:59 +0800

    init

---
+++ Util/Ocr.cs
@@ -0,0 +1,13 @@
+using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Util
+{
+    public class Ocr
+    {
+
+    }
+}
