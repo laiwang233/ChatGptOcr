310fde1e438f372993751ead671ee768da80a4c2
laiwang <2799199459@qq.com>
2023-04-04 15:00:10 +0800

    ocr识别完成

--- Util/Ocr.cs
+++ Util/Ocr.cs
@@ -1,13 +1,35 @@
 using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
-using System.Windows;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OpenCvSharp;
+using Sdcb.PaddleInference;
+using Sdcb.PaddleOCR;
+using Sdcb.PaddleOCR.Models;
+using Sdcb.PaddleOCR.Models.LocalV3;
 
 namespace Util
 {
     public class Ocr
     {
+        public string TextRecognition(Bitmap bitmap)
+        {
+            var model = LocalFullModels.ChineseV3;
+            using var all = new PaddleOcrAll(model, PaddleDevice.Mkldnn())
+            {
+                AllowRotateDetection = true, /* 允许识别有角度的文字 */
+                Enable180Classification = false, /* 允许识别旋转角度大于90度的文字 */
+            };
 
+            var memory = new MemoryStream();
+            bitmap.Save(memory, ImageFormat.Png);
+
+            using var src = Cv2.ImDecode(memory.GetBuffer(), ImreadModes.Color);
+            var result = all.Run(src);
+
+            return result.Text;
+        }
     }
 }
